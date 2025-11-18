ea2825b9f4bd225a784a969fe8c0b389bf76dbb1
JuveyGaymer XD <juvey746@gmail.com>
2025-11-17 17:16:13 -0800

    Se corrigen cambios en pantallas

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   const navigation = [
     { name: "Inicio", path: "/", icon: Home },
     { name: "Calendario", path: "/calendario", icon: Calendar },
-    // { name: "Ejercicios", path: "/ejercicios", icon: Activity },
+    { name: "Ejercicios", path: "/ejercicios", icon: Activity },
     // { name: "Chat con Doctores", path: "/chat", icon: MessageCircle },
     // { name: "Notificaciones", path: "/notificaciones", icon: Bell },
   ];
