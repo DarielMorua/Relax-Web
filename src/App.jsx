9ddcff1bc7c523b3f60d92737ff6966160c8ebd7
DarielMorua <12119@ulsachihuahua.edu.mx>
2025-11-21 16:18:55 -0600

    Se crea chat con datos mock

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Calendario from "./pages/Calendario.jsx";
 import Exercises from "./pages/Exercises.jsx";
 import ExerciseDetail from "./components/ExerciseDetail.jsx";
 import Notificaciones from "./pages/Norificaciones.jsx";
+import Chat from "./components/Chat.jsx";
 
 function App() {
   return (
@@ -29,7 +30,7 @@ function App() {
         />
 
         {/* Más rutas protegidas */}
-          <Route
+        <Route
           path="/calendario"
           element={
             <ProtectedRoute>
@@ -39,9 +40,7 @@ function App() {
             </ProtectedRoute>
           }
         />
-        
-       
-      {/*
+
         <Route
           path="/chat"
           element={
@@ -52,8 +51,7 @@ function App() {
             </ProtectedRoute>
           }
         />
-        */}
-        
+
         <Route
           path="/notificaciones"
           element={
@@ -63,7 +61,7 @@ function App() {
               </Layout>
             </ProtectedRoute>
           }
-        /> 
+        />
 
         {/* Ejercicios */}
         <Route
---
+++ src/mock/doctorsMock.js
@@ -0,0 +1,18 @@
+const doctors = [
+  {
+    id: 1,
+    name: "Dra. Idaly Cruz",
+    specialty: "Psiquiatra",
+    avatar: "👩‍⚕️",
+    online: true,
+  },
+  {
+    id: 2,
+    name: "Dr. José Chávez",
+    specialty: "Psicólogo",
+    avatar: "👨‍⚕️",
+    online: false,
+  },
+];
+
+export default doctors;
